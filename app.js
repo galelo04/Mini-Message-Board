4bf935ebe86d3468d8d585336f995cf61987d70e
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-01-27 18:04:12 +0200

    feat: finshed the mini message board

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const { env } = require('process');
 
-const indexRouter = require('./routes/indexRouter');
+const indexRouter = require('./routes/indexRouter').indexRouter;
 const newMessagesRouter = require('./routes/newMessagesRouter');
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(express.static(assetsPath));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/new', newMessagesRouter);
--- routes/newMessagesRouter.js
+++ routes/newMessagesRouter.js
@@ -1,5 +1,21 @@
 const { Router } = require('express');
 
 const newMessagesRouter = Router();
+const messages = require('./indexRouter').messages;
+
+newMessagesRouter.get('/', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+newMessagesRouter.post('/', (req, res) => {
+  const { user, text } = req.body;
+  const message = {
+    user,
+    text,
+    added: new Date(),
+  };
+  messages.push(message);
+  res.redirect('/');
+});
 
 module.exports = newMessagesRouter;
